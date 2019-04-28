4162cf11332fb2513a15896a332b459fdac305aa
Thomas von Deyen <thomas@vondeyen.com>
2019-04-28 23:54:36 +0200

    Add contents filter to element type
    Filter element contents by name

--- lib/alchemy/graphql/types/element_type.rb
+++ lib/alchemy/graphql/types/element_type.rb
@@ -9,7 +9,24 @@ module Alchemy
       field :page, Alchemy::GraphQL::PageType, null: false
       field :parent_element, self, null: true
       field :name, String, null: false
-      field :contents, [Alchemy::GraphQL::ContentType], null: false
+
+      field :contents, [Alchemy::GraphQL::ContentType], null: false do
+        description "Filter elements contents by their name. " \
+          "Either pass `only` to exclusively load Contents with this name or `except` to exclude these contents."
+        argument :only, String, required: false
+        argument :except, String, required: false
+      end
+
+      def contents(only: nil, except: nil)
+        contents = object.contents
+        if only
+          contents.where(name: only)
+        elsif except
+          contents.where.not(name: except)
+        else
+          contents
+        end
+      end
     end
   end
 end
