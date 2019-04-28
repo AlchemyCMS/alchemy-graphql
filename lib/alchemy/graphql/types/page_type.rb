1841cfdbbee9932cf10e36a4f5c013b715a18888
Thomas von Deyen <thomas@vondeyen.com>
2019-04-28 23:45:40 +0200

    Add elements filter to page type
    Filter page elements by name

--- lib/alchemy/graphql/types/page_type.rb
+++ lib/alchemy/graphql/types/page_type.rb
@@ -9,7 +9,24 @@ module Alchemy
       field :name, String, null: false
       field :title, String, null: false
       field :urlname, String, null: false
-      field :elements, [Alchemy::GraphQL::ElementType], null: false
+
+      field :elements, [Alchemy::GraphQL::ElementType], null: false do
+        description "Filter pages elements by their name. " \
+          "Either pass `only` to exclusively load Elements with this name or `except` to exclude these elements."
+        argument :only, String, required: false
+        argument :except, String, required: false
+      end
+
+      def elements(only: nil, except: nil)
+        elements = object.elements.available
+        if only
+          elements.where(name: only)
+        elsif except
+          elements.where.not(name: except)
+        else
+          elements
+        end
+      end
     end
   end
 end
