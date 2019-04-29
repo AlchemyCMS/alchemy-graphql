37a8b5c3469d9eac1f2ef868d1ea0a2fbd93b8cd
Thomas von Deyen <thomas@vondeyen.com>
2019-04-29 12:18:34 +0200

    Add Gem files to autoloads paths of dummy app
    makes it easier to develop

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -21,6 +21,9 @@ module Dummy
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Reload files from gem when they change
+    config.autoload_paths += [File.join(__dir__, '..', '..', '..', 'lib')]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
