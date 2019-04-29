560000c327ce7349c4f94f2717b5e631395c49c4
Thomas von Deyen <thomas@vondeyen.com>
2019-04-30 01:01:49 +0200

    Remove variable handling code from graphql controller
    We don't use that feature for now and it is untested

--- app/controllers/alchemy/graphql_controller.rb
+++ app/controllers/alchemy/graphql_controller.rb
@@ -10,7 +10,6 @@ module Alchemy
     def execute
       render json: Alchemy::GraphQL::Schema.execute(
         params[:query],
-        variables: ensure_hash(params[:variables]),
         context: {
           current_user: current_alchemy_user,
           current_ability: current_ability
@@ -24,24 +23,6 @@ module Alchemy
     end
 
     private
-
-    # Handle form data, JSON body, or a blank value
-    def ensure_hash(ambiguous_param)
-      case ambiguous_param
-      when String
-        if ambiguous_param.present?
-          ensure_hash(JSON.parse(ambiguous_param))
-        else
-          {}
-        end
-      when Hash, ActionController::Parameters
-        ambiguous_param
-      when nil
-        {}
-      else
-        raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
-      end
-    end
 
     def handle_error_in_development(e)
       logger.error e.message
